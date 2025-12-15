57d426f49993076b3ae610d67a709aa7efbe0f6a
abdellah <mariamsakina2010@gmail.com>
2025-12-15 02:27:18 +0100

    funciona en el hosting

--- src/utils/apiBase.js
+++ src/utils/apiBase.js
@@ -1,3 +1,7 @@
-const API_BASE = '';
+// src/utils/apiBase.js
+const API_BASE =
+  process.env.NODE_ENV === 'production'
+    ? 'https://wiki-data-qxef.onrender.com'
+    : 'http://localhost:5000';
 
 export default API_BASE;
